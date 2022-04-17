dd550afa26fd02a4f23e554acee2068fe13ad520
YukiY727 <83635663+YukiY727@users.noreply.github.com>
2022-04-17 10:34:30 +0900

    task_add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import "./styles.css";
 
 const onClickadd = () => {
+  // テキストボックスの値を取得し、初期化す津
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
-  alert(inputText);
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // divタグの子要素
+  div.appendChild(li);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
 };
 
 document
